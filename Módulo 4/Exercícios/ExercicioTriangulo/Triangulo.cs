5e07b9af1ebe477d8695f43a3b49d2efe7f3a2e0
George Wanderson <54476900+GeorgeWanderson@users.noreply.github.com>
2022-03-30 22:29:21 -0300

    Criado método para calcular a área do triângulo

--- Módulo 4/Exercícios/ExercicioTriangulo/Triangulo.cs
+++ Módulo 4/Exercícios/ExercicioTriangulo/Triangulo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ExercicioTriangulo
 {
     class Triangulo
@@ -6,5 +8,10 @@ namespace ExercicioTriangulo
         public double B;
         public double C;
 
+
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }

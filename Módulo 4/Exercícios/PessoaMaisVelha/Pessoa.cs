d7ffa839838356fbe4292adea4c69dc656ee8b0f
George Wanderson <54476900+GeorgeWanderson@users.noreply.github.com>
2022-03-30 22:19:24 -0300

    Exercício de Fixação - Pessoa mais Velha
    Cadastro simples de 2 usuários, após informar os dados, o programa compara a idade e exibe no console quem é mais velho.

---
+++ Módulo 4/Exercícios/PessoaMaisVelha/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace PessoaMaisVelha
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        
+    }
+}
---
+++ Módulo 4/Exercícios/PessoaMaisVelha/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PessoaMaisVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            System.Console.WriteLine("Dados da primeira pessoa:");
+            p1.Nome = Console.ReadLine();
+            System.Console.WriteLine("Idade:");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Dados da segunda pessoa:");
+            p2.Nome = Console.ReadLine();
+            System.Console.WriteLine("Idade");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade) {
+                System.Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else {
+                System.Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+
+
+        }
+    }
+}

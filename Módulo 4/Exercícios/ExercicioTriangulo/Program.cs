a22e5b509cfc030e44a08270743899dc810de495
George Wanderson <54476900+GeorgeWanderson@users.noreply.github.com>
2022-03-30 22:21:06 -0300

    Exercício de Fixação - Triângulo

---
+++ Módulo 4/Exercícios/ExercicioTriangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Área de y = " + areaY.ToString("F4"), CultureInfo.InvariantCulture);
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
